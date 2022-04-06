d33d7c5aad13321f05bb4cd9ae09353b0afd91f0
himanshudhimanux <71764209+himanshudhimanux@users.noreply.github.com>
2022-04-06 23:16:46 +0530

    deploy noteit mern app

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const userRoutes = require('./routes/userRoutes');
 const noteRoutes = require('./routes/noteRoutes');
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
 const app = express();
 app.use(cors());
@@ -14,18 +15,27 @@ connectDB();
 app.use(express.json());
 
 
+app.use('/api/users',userRoutes);
+app.use('/api/notes',noteRoutes);
 
-// app.get("/", (req,res) =>{
-//     res.send("API is running");
-// });
+// ------------ Deployment function -----------
 
-// app.get("/api/notes", (req,res) =>{
-//     res.json(notes)
-// })
+__dirname=path.resolve();
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"frontend/build")));
 
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,"frontend","build","index.html"));
+    });
 
-app.use('/api/users',userRoutes)
-app.use('/api/notes',noteRoutes)
+}else{
+    app.get("/", (res,req) => {
+        res.send("API is running");
+    });
+}
+
+// ------------ Deployment function -----------
+
 
 app.use(notFound);
 app.use(errorHandler);
