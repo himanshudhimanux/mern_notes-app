11dc8ba7e6d8155740925c18b189be1d5725eff6
himanshudhimanux <71764209+himanshudhimanux@users.noreply.github.com>
2022-04-07 22:39:23 +0530

    make it better

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -18,21 +18,23 @@ const LandingPage = () => {
       <>
     <div className='landing-page'>
         <div className='container'>
-            <div className='row justify-content-center'>
-                <div className='col-md-8 col-xs-12 col-sm-12'>
-                    <div className='Landing-page-title'>
-                        <h1>Welcome to Note-it</h1>
-                    </div>
-                    <div className='Landing-page-subtitle'>
-                        <p>One Safe Place for all Your Notes</p>
+            <div className='content-card'>
+                <div className='row justify-content-center'>
+                    <div className='col-md-8 col-xs-12 col-sm-12'>
+                        <div className='Landing-page-title'>
+                            <h1>Welcome to Note-it</h1>
+                        </div>
+                        <div className='Landing-page-subtitle'>
+                            <p>One Safe Place for all Your Notes</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='row justify-content-center'>
-                <div className='col-md-6 col-xs-12 col-sm-12'>
-                    <div className='lading-page-buttons'>
-                        <Link to="/login" className='btn btn-lg btn-primary'>LOG IN</Link>
-                        <Link to="/register" className='btn btn-lg btn-outline-primary'>REGISTER</Link>
+                <div className='row justify-content-center'>
+                    <div className='col-md-6 col-xs-12 col-sm-12'>
+                        <div className='lading-page-buttons'>
+                            <Link to="/login" className='btn btn-lg btn-primary'>LOG IN</Link>
+                            <Link to="/register" className='btn btn-lg btn-outline-primary'>REGISTER</Link>
+                        </div>
                     </div>
                 </div>
             </div>
